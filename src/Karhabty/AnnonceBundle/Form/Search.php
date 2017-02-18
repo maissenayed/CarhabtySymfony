87e5c99b0d9041834950b190f6a77602d87d58b0
Maissen Ayed <ayed.maissen@gmail.com>
2017-02-18 16:46:14 +0100

    maissen calander,sweetalert ,annonce crud and some controlle not final2

--- src/Karhabty/AnnonceBundle/Form/Search.php
+++ src/Karhabty/AnnonceBundle/Form/Search.php
@@ -2,17 +2,13 @@
 
 namespace Karhabty\AnnonceBundle\Form;
 
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 
-class AnnonceType extends AbstractType
+class Search extends AbstractType
 {
     /**
      * {@inheritdoc}
@@ -20,14 +16,10 @@ class AnnonceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('title')
-            ->add('AnneeDeProduit',DateType::class, ['widget' => 'single_text', 'format' => 'dd-MM-yyyy'])
-            ->add('Model')
-            ->add('Marque')
-            ->add('Region')
+
             ->add('Ville')
-            ->add('Paye')
-            ->add('Descreption')
-            ->add('Prix',IntegerType::class)
+
+            ->add('Prix')
             ->add('Category', ChoiceType::class, array(
                 'choices'  => array(
                     'accessoire' => 'accessoire',
@@ -37,16 +29,10 @@ class AnnonceType extends AbstractType
 
 
 
-            ->add('imageFile',VichFileType::class,array('label' => 'upload file',
-                    'attr'=> array('class'=> 'fileContainer'),
-                    'required' => false,
-                    'allow_delete' => true, // not mandatory, default is true
-                    'download_link' => true, // not mandatory, default is true
 
-            ))
         ;
     }
-    
+
     /**
      * {@inheritdoc}
      */
